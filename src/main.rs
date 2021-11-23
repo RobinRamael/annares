fd58c97ea7e97e6e01000c4716bef4f5ea9a04f7
Robin Ramael <robin.ramael@gmail.com>
2021-11-23 20:36:45 +0100

    sort of?

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,10 @@ impl PeeringNode for MyPeeringNode {
     ) -> Result<Response<IntroductionReply>, Status> {
         let new_peer_addr = request.into_inner().sender_adress;
 
+        println!("received introduction from {:?}", new_peer_addr);
+
         let mut locked_peers = self.known_peers.lock().unwrap();
+        println!("I currently know {:?}", locked_peers);
 
         // format peer socket addrs into strings
         let peer_addrs = locked_peers
@@ -37,6 +40,8 @@ impl PeeringNode for MyPeeringNode {
             .collect();
 
         locked_peers.insert(new_peer_addr.parse().unwrap());
+
+        println!("known peers: {:?}", locked_peers);
 
         Ok(Response::new(IntroductionReply {
             known_peers: peer_addrs,
@@ -48,12 +53,6 @@ fn build_grpc_url(addr: SocketAddr) -> String {
     format!("http://{}", addr)
 }
 
-fn spawn_introduce_myself<'a>(node: &'static MyPeeringNode, peer: SocketAddr) {
-    tokio::spawn(async move {
-        node.introduce_myself(peer).await;
-    });
-}
-
 impl MyPeeringNode {
     fn new(port: u16, known_peers: HashSet<SocketAddr>) -> MyPeeringNode {
         MyPeeringNode {
@@ -61,34 +60,54 @@ impl MyPeeringNode {
             known_peers: Arc::new(Mutex::new(known_peers)),
         }
     }
+}
 
-    async fn introduce_myself(
-        &self,
-        bootstrap_addr: SocketAddr,
-    ) -> Result<(), tonic::transport::Error> {
-        let mut client = PeeringNodeClient::connect(build_grpc_url(bootstrap_addr)).await?;
+async fn send_introduction(
+    source_addr: SocketAddr,
+    target_addr: SocketAddr,
+) -> Result<HashSet<SocketAddr>, tonic::transport::Error> {
+    let mut client = PeeringNodeClient::connect(build_grpc_url(target_addr)).await?;
 
-        let request = tonic::Request::new(IntroductionRequest {
-            sender_adress: self.addr.to_string(),
-        });
+    let request = tonic::Request::new(IntroductionRequest {
+        sender_adress: source_addr.to_string(),
+    });
 
-        let response = client.introduce(request).await.unwrap();
+    let response = client.introduce(request).await.unwrap();
 
-        let mut locked_peers = self.known_peers.lock().unwrap();
+    let new_peers = response
+        .get_ref()
+        .known_peers
+        .iter()
+        .filter_map(|addr_string| addr_string.parse::<SocketAddr>().ok())
+        .collect();
 
-        response
-            .get_ref()
-            .known_peers
-            .iter()
-            .filter_map(|addr_string| addr_string.parse::<SocketAddr>().ok())
-            .for_each(|new_peer| {
-                if locked_peers.insert(new_peer) {
-                    spawn_introduce_myself(self, new_peer);
-                };
+    Ok(new_peers)
+}
+
+async fn mingle(
+    source_addr: SocketAddr,
+    bootstrap_addr: SocketAddr,
+    known_peers: HashSet<SocketAddr>,
+) -> Result<HashSet<SocketAddr>, tonic::transport::Error> {
+    // let locked_peers = known_peers.lock().unwrap();
+
+    println!("mingling...");
+    dbg!(bootstrap_addr, known_peers);
+    let new_peers: HashSet<SocketAddr> = send_introduction(source_addr, bootstrap_addr)
+        .await?
+        .into_iter()
+        .map(|peer| {
+            tokio::task::spawn(async move {
+                println!("sending intro to {:?}", peer);
+                send_introduction(source_addr, peer).await.unwrap();
             });
+            peer
+        })
+        .collect();
 
-        Ok(())
-    }
+    dbg!(&new_peers);
+
+    Ok(new_peers)
 }
 
 fn ipv6_loopback_socketaddr(port: u16) -> SocketAddr {
@@ -107,20 +126,25 @@ struct Cli {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::from_args();
 
-    let my_peering_node;
+    let peers;
+
+    dbg!(&args);
 
     if args.bootstrap_peer.is_some() {
-        my_peering_node = MyPeeringNode::new(
-            args.port,
-            HashSet::from_iter(vec![args.bootstrap_peer.unwrap()]),
-        );
-
-        my_peering_node
-            .introduce_myself(args.bootstrap_peer.unwrap())
-            .await?;
+        let peer = args.bootstrap_peer.unwrap();
+        peers = mingle(
+            ipv6_loopback_socketaddr(args.port),
+            peer,
+            HashSet::from_iter(vec![peer]),
+        )
+        .await?;
     } else {
-        my_peering_node = MyPeeringNode::new(args.port, HashSet::new());
+        peers = HashSet::new()
     }
+
+    let my_peering_node = MyPeeringNode::new(args.port, peers);
+
+    println!("awaiting further introductions...");
 
     Server::builder()
         .add_service(PeeringNodeServer::new(my_peering_node))
