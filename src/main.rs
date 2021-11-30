8caacf1e19ccf540e15c03a1e594c9c7130016f2
Robin Ramael <robin.ramael@gmail.com>
2021-11-30 14:04:47 +0100

    more homti

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use std::net::SocketAddr;
 use structopt::StructOpt;
+use std::sync::Arc;
 
 mod peering;
 
 use peering::grpc::peering_node_server::PeeringNodeServer;
 
-use peering::node::{KnownPeer, MyPeeringNode};
+use peering::node::{print_peers, KnownPeer, MyPeeringNode, NodeData};
 use peering::utils;
 
 use std::collections::HashSet;
 use tonic::transport::Server;
+
+use tokio::time::Duration;
 
 #[derive(StructOpt, Debug)]
 struct Cli {
@@ -32,16 +35,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Running on port {:?}", args.port);
     println!("bootstrapping using {:?}", args.bootstrap_peer);
     println!("");
-    let my_peering_node = MyPeeringNode::new(args.port, peers);
+
+
+    let my_data = Arc::new(NodeData::new(args.port, peers));
+
+    let my_peering_node = MyPeeringNode::new(my_data);
 
     my_peering_node.mingle().await?;
 
+    // let peer_check = &my_peering_node.initialize_peer_check(Duration::from_secs(10));
+
+    print_peers(&my_peering_node.data.known_peers.read().unwrap());
+
     println!("awaiting further introductions...");
 
-    Server::builder()
+    let server = Server::builder()
         .add_service(PeeringNodeServer::new(my_peering_node))
         .serve(utils::ipv6_loopback_socketaddr(args.port))
         .await?;
+
 
     Ok(())
 }
--- src/peering/node.rs
+++ src/peering/node.rs
@@ -9,23 +9,126 @@ use crate::peering::hash::Hash;
 use crate::peering::utils;
 
 use std::collections::{HashMap, HashSet};
-use std::fmt::{Display, Debug};
+use std::fmt::{Debug, Display};
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex, RwLock};
+use std::time::{Duration, Instant};
 use tonic::{Request, Response, Status};
+
+use tokio::time::Interval;
+use tokio::task;
+
+#[derive(Debug)]
+pub struct MyPeeringNode {
+    pub data: Arc<NodeData>,
+}
+
+#[derive(Debug)]
+pub struct NodeData {
+    pub addr: SocketAddr,
+    pub hash: Hash,
+    pub known_peers: Arc<RwLock<HashSet<KnownPeer>>>,
+    pub data_shard: Arc<RwLock<HashMap<Hash, String>>>,
+}
 
 #[derive(PartialEq, Eq, std::hash::Hash, Clone)]
 pub struct KnownPeer {
     pub addr: SocketAddr,
     pub hash: Hash,
+    pub last_contact: Instant,
 }
 
-#[derive(Debug)]
-pub struct MyPeeringNode {
-    pub addr: SocketAddr,
-    pub hash: Hash,
-    pub known_peers: Arc<Mutex<HashSet<KnownPeer>>>,
-    pub data_shard: Arc<RwLock<HashMap<Hash, String>>>,
+
+impl NodeData {
+    pub fn new(port: u16, known_peers: HashSet<KnownPeer>) -> NodeData {
+        let addr = utils::ipv6_loopback_socketaddr(port);
+        NodeData {
+            addr,
+            hash: Hash::hash(addr.to_string()),
+            known_peers: Arc::new(RwLock::new(known_peers)),
+            data_shard: Arc::new(RwLock::new(HashMap::new())),
+        }
+    }
+}
+
+impl MyPeeringNode {
+    pub fn new(data: Arc<NodeData>) -> MyPeeringNode {
+        MyPeeringNode {
+            data
+        }
+    }
+
+    pub async fn mingle(&self) -> Result<(), tonic::transport::Error> {
+        // let data = self.data.to_owned();
+        let mut locked_peers = self.data.known_peers.write().unwrap();
+
+        let mut all_new_peers: HashSet<KnownPeer> = HashSet::new();
+
+        for known_peer in locked_peers.iter() {
+            let new_peers = self.send_introduction(&known_peer.addr).await?;
+
+            print_peers(&new_peers);
+
+            for new_peer in new_peers.iter().filter(|p| p.addr != self.data.addr) {
+                self.send_introduction(&new_peer.addr).await?;
+            }
+
+            all_new_peers.extend(new_peers.clone());
+        }
+
+        locked_peers.extend(all_new_peers);
+
+        Ok(())
+    }
+
+    async fn send_introduction(
+        &self,
+        target_addr: &SocketAddr,
+    ) -> Result<HashSet<KnownPeer>, tonic::transport::Error> {
+        let target_url = utils::build_grpc_url(target_addr);
+
+        println!("sending intro to {}", target_addr);
+
+        let mut client = PeeringNodeClient::connect(target_url).await?;
+
+        let request = tonic::Request::new(IntroductionRequest {
+            sender_adress: self.data.addr.to_string(),
+        });
+
+        let response = client.introduce(request).await.unwrap();
+
+        let new_peers = response
+            .into_inner()
+            .known_peers
+            .into_iter()
+            .filter_map(|peer| peer.try_into().ok())
+            .collect();
+
+        Ok(new_peers)
+    }
+
+    pub async fn initialize_peer_check(&self, interval: Duration) -> task::JoinHandle<()> {
+        let forever = tokio::task::spawn(async move {
+            println!("in task");
+            let mut interval = tokio::time::interval(interval);
+
+            loop {
+                interval.tick().await;
+                println!("tick");
+            }
+        });
+
+        forever
+    }
+
+    fn oldest_contact(&self) -> Option<KnownPeer> {
+        self.data.known_peers
+            .read()
+            .unwrap()
+            .clone()
+            .into_iter()
+            .max_by_key(|p| p.last_contact)
+    }
 }
 
 #[tonic::async_trait]
@@ -38,16 +141,16 @@ impl PeeringNode for MyPeeringNode {
 
         println!("received introduction from {:?}", new_peer_addr);
 
-        let mut locked_peers = self.known_peers.lock().unwrap();
-
-        locked_peers.insert(KnownPeer::new(new_peer_addr.parse().unwrap()));
-
-        print_peers(&locked_peers);
+        let mut locked_peers = self.data.known_peers.write().unwrap();
 
         let known_peers = locked_peers
             .iter()
             .map(|p| grpc::Peer::from(p.clone()))
             .collect();
+
+        locked_peers.insert(KnownPeer::new(new_peer_addr.parse().unwrap()));
+
+        print_peers(&locked_peers);
 
         Ok(Response::new(IntroductionReply { known_peers }))
     }
@@ -56,10 +159,10 @@ impl PeeringNode for MyPeeringNode {
         &self,
         _: Request<ListPeersRequest>,
     ) -> Result<Response<ListPeersReply>, Status> {
-        let mut peers = self.known_peers.lock().unwrap().clone();
+        let mut peers = self.data.known_peers.read().unwrap().clone();
 
         // include this node as well
-        peers.insert(KnownPeer::new(self.addr));
+        peers.insert(KnownPeer::new(self.data.addr));
 
         let ps = peers.iter().map(|p| p.clone().into()).collect();
 
@@ -83,6 +186,7 @@ impl KnownPeer {
         KnownPeer {
             addr,
             hash: Hash::hash(addr.to_string()),
+            last_contact: Instant::now(),
         }
     }
 }
@@ -102,93 +206,21 @@ impl From<KnownPeer> for grpc::Peer {
     }
 }
 
-pub enum PeerFromGRPCError {
-    Hex(hex::FromHexError),
-    Addr(std::net::AddrParseError),
-}
-
 impl TryFrom<grpc::Peer> for KnownPeer {
-    type Error = PeerFromGRPCError;
+    type Error = std::net::AddrParseError;
 
     fn try_from(grpc_peer: grpc::Peer) -> Result<Self, Self::Error> {
-        let addr = match grpc_peer.addr.parse::<SocketAddr>() {
-            Err(e) => return Err(PeerFromGRPCError::Addr(e)),
-            Ok(a) => a,
-        };
+        let addr = grpc_peer.addr.parse::<SocketAddr>()?;
 
-        let hash = match grpc_peer.hash.try_into() {
-            Err(e) => return Err(PeerFromGRPCError::Hex(e)),
-            Ok(h) => h,
-        };
-
-        Ok(KnownPeer { addr, hash })
+        Ok(KnownPeer::new(addr))
     }
 }
-
-impl MyPeeringNode {
-    pub fn new(port: u16, known_peers: HashSet<KnownPeer>) -> MyPeeringNode {
-        let addr = utils::ipv6_loopback_socketaddr(port);
-        MyPeeringNode {
-            addr,
-            hash: Hash::hash(addr.to_string()),
-            known_peers: Arc::new(Mutex::new(known_peers)),
-            data_shard: Arc::new(RwLock::new(HashMap::new())),
-        }
-    }
-
-    pub async fn mingle(&self) -> Result<(), tonic::transport::Error> {
-        let mut locked_peers = self.known_peers.lock().unwrap();
-
-        let mut all_new_peers: HashSet<KnownPeer> = HashSet::new();
-
-        for known_peer in locked_peers.iter() {
-
-            let new_peers = self.send_introduction(&known_peer.addr).await?;
-
-            print_peers(&new_peers);
-
-            for new_peer in new_peers.iter().filter(|p| p.addr != self.addr) {
-                self.send_introduction(&new_peer.addr).await?;
-            }
-
-            all_new_peers.extend(new_peers.clone());
-
-        }
-        locked_peers.extend(all_new_peers);
-
-        Ok(())
-    }
-
-    async fn send_introduction(
-        &self,
-        target_addr: &SocketAddr,
-    ) -> Result<HashSet<KnownPeer>, tonic::transport::Error> {
-        let target_url = utils::build_grpc_url(target_addr);
-        dbg!(&target_url);
-
-        let mut client = PeeringNodeClient::connect(target_url).await?;
-
-        let request = tonic::Request::new(IntroductionRequest {
-            sender_adress: self.addr.to_string(),
-        });
-
-        let response = client.introduce(request).await.unwrap();
-
-        let new_peers = response
-            .into_inner()
-            .known_peers
-            .into_iter()
-            .filter_map(|peer| peer.try_into().ok())
-            .collect();
-
-        Ok(new_peers)
-    }
-}
-
 
 impl Debug for KnownPeer {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        f.debug_struct("KnownPeer").field("addr", &self.addr).finish()
+        f.debug_struct("KnownPeer")
+            .field("addr", &self.addr)
+            .finish()
     }
 }
 
