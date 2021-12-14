9ea041bfc0716b34bf0d7bc5ced5291f9b8a720d
Robin Ramael <robin.ramael@gmail.com>
2021-12-14 14:19:11 +0100

    add and cyclic add for keys
    my brain hurts

--- src/keys/hash.rs
+++ src/keys/hash.rs
@@ -1,4 +1,5 @@
 use generic_array::{ArrayLength, GenericArray};
+use num::pow;
 use std::cmp;
 use std::cmp::Ordering;
 use std::ops;
@@ -121,11 +122,38 @@ where
         Arru8::new(one)
     }
 
+    fn halfway() -> Self {
+        let mut halfway = GenericArray::default();
+        halfway[N::to_usize() - 1] = 128;
+        Arru8::new(halfway)
+    }
+
+    fn two_to_the_power_of(k: usize) -> Self {
+        let mut res = GenericArray::default();
+        let m = k % 8;
+        let idx = (k - m) / 8;
+        // assert!(m <= 4);
+        res[idx] = pow(2u8, m);
+        Arru8::new(res)
+    }
+
     fn cyclic_sub(self, rhs: Self) -> Self {
         if self.ge(&rhs) {
             self - rhs
         } else {
             Self::max() - (rhs - self - Self::one())
+        }
+    }
+
+    fn cyclic_add(self, rhs: Self) -> Self {
+        if self > rhs {
+            rhs.cyclic_add(self)
+        } else {
+            if self <= Self::max() - rhs {
+                self + rhs
+            } else {
+                self - (Self::max() - rhs + Self::one())
+            }
         }
     }
 
@@ -216,6 +244,53 @@ where
 
             result.arr[i] = d;
         }
+
+        result
+    }
+}
+
+fn carry_add(a: u8, b: u8, carry: u8) -> (u8, u8) {
+    dbg!((a, b, carry));
+    assert!(carry == 1 || carry == 0);
+    // holy branching, batman!
+    if a > b {
+        return carry_add(b, a, carry);
+    }
+
+    if carry == 1 {
+        if a == 255 {
+            return (255, 1);
+        } else {
+            return carry_add(a + 1, b, 0);
+        }
+    }
+
+    if a <= 255 - b {
+        (a + b, 0)
+    } else {
+        (a - (255 - b + 1), 1)
+    }
+}
+
+impl<N> ops::Add for Arru8<N>
+where
+    N: ArrayLength<u8> + Eq + Copy,
+    <N as ArrayLength<u8>>::ArrayType: Copy,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        let mut carry: u8 = 0;
+        let mut result: Self = Arru8::new(GenericArray::default());
+
+        for (i, (a, b)) in self.arr.into_iter().zip(rhs.arr.into_iter()).enumerate() {
+            let (s, new_carry) = carry_add(a, b, carry);
+
+            result.arr[i] = s;
+            carry = new_carry;
+        }
+
+        assert!(carry == 0, "Overflow!");
 
         result
     }
@@ -424,6 +499,102 @@ mod tests {
         let n_m = Arru8::new(arr![u8; 17, 243]);
 
         assert!(n_m.in_cyclic_range(&n_1, &n_2));
+    }
+
+    #[test]
+    fn test_2pow() {
+        assert_eq!(
+            Arru8::<U3>::two_to_the_power_of(0),
+            Arru8::new(arr![u8; 1, 0, 0])
+        );
+        assert_eq!(
+            Arru8::<U3>::two_to_the_power_of(1),
+            Arru8::new(arr![u8; 2, 0, 0])
+        );
+        assert_eq!(
+            Arru8::<U3>::two_to_the_power_of(2),
+            Arru8::new(arr![u8; 4, 0, 0])
+        );
+        assert_eq!(
+            Arru8::<U3>::two_to_the_power_of(7),
+            Arru8::new(arr![u8; 128, 0, 0])
+        );
+        assert_eq!(
+            Arru8::<U3>::two_to_the_power_of(8),
+            Arru8::new(arr![u8; 0, 1, 0])
+        );
+        assert_eq!(
+            Arru8::<U3>::two_to_the_power_of(9),
+            Arru8::new(arr![u8; 0, 2, 0])
+        );
+        assert_eq!(
+            Arru8::<U3>::two_to_the_power_of(16),
+            Arru8::new(arr![u8; 0, 0, 1])
+        );
+        assert_eq!(
+            Arru8::<U3>::two_to_the_power_of(23),
+            Arru8::new(arr![u8; 0, 0, 128])
+        );
+    }
+
+    #[test]
+    fn test_carry_add() {
+        assert_eq!(carry_add(1, 1, 0), (2, 0));
+        assert_eq!(carry_add(1, 1, 1), (3, 0));
+
+        assert_eq!(carry_add(200, 60, 0), (4, 1));
+        assert_eq!(carry_add(200, 60, 1), (5, 1));
+        assert_eq!(carry_add(60, 200, 0), (4, 1));
+        assert_eq!(carry_add(60, 200, 1), (5, 1));
+
+        assert_eq!(carry_add(0, 0, 0), (0, 0));
+        assert_eq!(carry_add(0, 0, 1), (1, 0));
+
+        assert_eq!(carry_add(255, 255, 0), (254, 1));
+        assert_eq!(carry_add(255, 255, 1), (255, 1));
+
+        assert_eq!(carry_add(0, 254, 1), (255, 0));
+    }
+
+    #[test]
+    fn test_add() {
+        assert_eq!(
+            Arru8::new(arr![u8; 0, 0, 2]) + Arru8::new(arr![u8; 0, 0, 1]),
+            Arru8::new(arr![u8; 0, 0, 3])
+        );
+
+        assert_eq!(
+            Arru8::new(arr![u8; 200, 0, 2]) + Arru8::new(arr![u8; 60, 0, 1]),
+            Arru8::new(arr![u8; 4, 1, 3])
+        );
+
+        assert_eq!(
+            Arru8::new(arr![u8; 255, 255, 254]) + Arru8::new(arr![u8; 255, 255, 0]),
+            Arru8::new(arr![u8; 254, 255, 255])
+        );
+    }
+
+    #[test]
+    fn test_cyclic_add() {
+        assert_eq!(
+            Arru8::new(arr![u8; 0, 0, 2]).cyclic_add(Arru8::new(arr![u8; 0, 0, 1])),
+            Arru8::new(arr![u8; 0, 0, 3])
+        );
+
+        assert_eq!(
+            Arru8::new(arr![u8; 1, 2, 200]).cyclic_add(Arru8::new(arr![u8; 4, 10, 60])),
+            Arru8::new(arr![u8; 5, 12, 4])
+        );
+
+        assert_eq!(
+            Arru8::new(arr![u8; 0, 0, 255]).cyclic_add(Arru8::new(arr![u8; 0, 0, 1])),
+            Arru8::new(arr![u8; 0, 0, 0])
+        );
+
+        assert_eq!(
+            Arru8::new(arr![u8; 255, 255, 255]).cyclic_add(Arru8::new(arr![u8; 255, 255, 255])),
+            Arru8::new(arr![u8; 254, 255, 255])
+        );
     }
 
     extern crate quickcheck;
